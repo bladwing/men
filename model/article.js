00d670ed8716b5986ccb73a929c6a62bbe78c1cd
bladwing <timoti.ponarenko@gmail.com>
2023-02-02 23:15:25 +0400

    add category

--- model/article.js
+++ model/article.js
@@ -5,17 +5,15 @@ const { Schema } = mongoose;
 
 let articleSchema = new Schema(
   {
-    title: {
-      type: String,
-    },
-    article: {
-      type: String,
-    },
+    title: { type: String },
+    article: { type: String },
+    category: { type: String },
   },
   {
     collection: "article",
-  }
+    timestamps: true 
+  },
+
 );
- 
 
 export default mongoose.model("Article", articleSchema);
